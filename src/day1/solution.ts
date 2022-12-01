b9daa2b53070445bd24fd8653068c97b4eed15df
Albrecht Schmidt <mail@albrechtschmidt.de>
2022-12-01 13:49:31 +0100

    Generic with parsedType

--- src/day1/solution.ts
+++ src/day1/solution.ts
@@ -16,7 +16,7 @@ function sortByCalories(elfs: Elf[]): Elf[] {
   return elfs.sort((a, b) => b.sumOfCarriedCalories - a.sumOfCarriedCalories);
 }
 
-export default class Solution extends AbstractSolution {
+export default class Solution extends AbstractSolution<Elf[]> {
   parseInput(input: string[]): Elf[] {
     return input.reduce(
       (result: Elf[], item: string) => {
@@ -35,17 +35,12 @@ export default class Solution extends AbstractSolution {
     );
   }
 
-  public solveFirst(input: string[]): string {
-    const elfes = sortByCalories(this.parseInput(input));
+  public solveFirst(input: Elf[]): string {
+    const elfes = sortByCalories(input);
     return elfes[0]?.sumOfCarriedCalories + "";
   }
-  public solveSecond(input: string[]): string {
-    const elfes = sortByCalories(this.parseInput(input)) as [
-      Elf,
-      Elf,
-      Elf,
-      ...Elf[]
-    ];
+  public solveSecond(input: Elf[]): string {
+    const elfes = sortByCalories(input) as [Elf, Elf, Elf, ...Elf[]];
     return (
       elfes[0].sumOfCarriedCalories +
       elfes[1].sumOfCarriedCalories +
--- src/utils/runner.ts
+++ src/utils/runner.ts
@@ -1,45 +1,48 @@
 import { readFileSync, writeFileSync } from "fs";
 
-export interface SolutionInterface {
-  solveFirst: (input: string[]) => string;
-  solveSecond: (input: string[]) => string;
+export interface SolutionInterface<ParsedInput> {
+  solveFirst: (input: ParsedInput) => string;
+  solveSecond: (input: ParsedInput) => string;
 }
 
-export abstract class AbstractSolution implements SolutionInterface {
+export abstract class AbstractSolution<ParsedInput>
+  implements SolutionInterface<ParsedInput>
+{
   public folder: string;
 
-  public abstract solveFirst(input: string[]): string;
-  public abstract solveSecond(input: string[]): string;
+  public abstract parseInput(input: string[]): ParsedInput;
+  public abstract solveFirst(input: ParsedInput): string;
+  public abstract solveSecond(input: ParsedInput): string;
 
   getInput(): string[] {
     return readFileSync(this.folder + "input.txt", "utf-8").split(/\r?\n/);
   }
 
   runFirst(): string {
-    const result = this.solveFirst(this.getInput());
+    const result = this.solveFirst(this.parseInput(this.getInput()));
     writeFileSync(this.folder + "solution1.txt", result);
     return result;
   }
 
   runSecond(): string {
-    const result = this.solveSecond(this.getInput());
+    const result = this.solveSecond(this.parseInput(this.getInput()));
     writeFileSync(this.folder + "solution2.txt", result);
     return result;
   }
 }
 
-async function loadSolution(day: string): Promise<AbstractSolution> {
-  const solutionModule: { default: { new (): AbstractSolution } } =
+async function loadSolution<T>(day: string): Promise<AbstractSolution<T>> {
+  const solutionModule: { default: { new (): AbstractSolution<T> } } =
     await import(`../day${day}/solution`);
   const { default: solutionClass } = solutionModule;
   const solution = new solutionClass();
   return solution;
 }
 
-export function run(day: string): void {
+export function run<T>(day: string): void {
   const folder = `./src/day${day}/`;
 
-  loadSolution(day).then((solution: AbstractSolution) => {
+  loadSolution(day).then((solution: AbstractSolution<T>) => {
     solution.folder = folder;
     console.log(solution.runFirst());
     console.log(solution.runSecond());
